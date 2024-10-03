683661022a59ae3f0443f562caedbb2ac095d43c
Simranjeet Singh <simran@monarch.is>
2024-10-03 12:37:41 +0530

    deploy commit

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -50,7 +50,7 @@ const Blog = () => {
                   <div className="h-2.5 bg-body-color bg-opacity-[20%] rounded-full dark:bg-gray-700 w-48 mb-4"></div>
                   <div className="h-2 bg-body-color bg-opacity-[20%] rounded-full dark:bg-gray-700 max-w-[480px] mb-2.5"></div>
                   <div className="h-2 bg-body-color bg-opacity-[20%] rounded-full dark:bg-gray-700 mb-2.5"></div>
-                  <div className="h-2 bg-body-color bg-opacity-[20%] rounded-full dark:bg-gray-700 max-w-[440px] mb-2.5"></div>
+                  <div className="h-2 bg-body-color  bg-opacity-[20%] rounded-full dark:bg-gray-700 max-w-[440px] mb-2.5"></div>
                   <div className="h-2 bg-body-color bg-opacity-[20%] rounded-full dark:bg-gray-700 max-w-[460px] mb-2.5"></div>
                   <div className="h-2 bg-body-color bg-opacity-[20%] rounded-full dark:bg-gray-700 max-w-[360px]"></div>
                 </div>
